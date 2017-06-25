9ef74b9f16bec43ac3c475f67be02332735d1b71
Taelss <coobtroob@gmail.com>
2017-06-24 22:05:10 -0700

    added nospread

--- sploits.lua
+++ sploits.lua
@@ -4,15 +4,14 @@ chat.AddText( Color( 255, 200, 255 ), ply, "[sploits] ", Color(255,100,50), "Loa
 surface.PlaySound ("UI/buttonclick.wav")
 
 // --- cool watermark ---
-
+p
 hook.Add("PostDrawHUD", "", function()
 	surface.SetFont("DermaLarge");
 	surface.SetTextColor(65, 255, 65);
 	surface.SetTextPos(5, 5);
 	surface.DrawText("sploits - discord.gg/J3pxPVe");
 	
-end)
-
+endp
 
 
 
@@ -126,7 +125,26 @@ end)
 
 
 
+// --- no spread ---
 
+local function NoSpread()    
+if GetConVarNumber("sploits_nospread") == 1 and LocalPlayer().GetActiveWeapon != nil then
+local wep = LocalPlayer():GetActiveWeapon()
+if wep.data then
+wep.data.Recoil = 0
+wep.data.Cone = 0
+wep.data.Spread = 0
+end
+if wep.Primary then
+wep.Primary.Recoil = 0
+wep.Primary.Cone = 0
+wep.Primary.Spread = 0
+end
+end
+end
+hook.Add("Tick", "NoSpread", NoSpread)
+
+CreateClientConVar( "sploits_nospread", 0, true, false)
 
 
 
